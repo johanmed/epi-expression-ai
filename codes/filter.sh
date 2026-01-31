d4162a2be04b56d1ad4deac8b26d03b0ff987822
johannesm <johannesm@jom.org>
2026-01-31 14:51:21 +0300

    Add missing step

--- codes/filter.sh
+++ codes/filter.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cd output
+grep -v chr *$1*assoc* | awk '$11 < 1e-06 {print $0}' > summary_$1.txt
 cut -f 1,2 summary_more_$1.txt | cut -d ':' -f 2 | sort| uniq > intermediate_more_$1.txt
