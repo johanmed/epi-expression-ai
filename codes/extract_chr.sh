a9bef54635ef3d27669781a5b132eab91ec7cdfe
johannesm <johannesm@jom.org>
2026-02-06 08:28:07 +0300

    Perform functional analysis for AI results

--- codes/extract_chr.sh
+++ codes/extract_chr.sh
@@ -2,4 +2,4 @@
 
 cd output
 n=$1
-cut -f 1,2 conserved_sig.txt | awk '$1 == $n {print $0}' | cut -f 2 | sed -z 's/\n/,/g' > chr${n}_markers.txt
+cut -f 1,2,3 conserved_sig.txt | awk '$1 == $n {print $0}' | cut -f 3 | sed -z 's/\n/,/g' > chr${n}_pos.txt
