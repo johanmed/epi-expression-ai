0925ff08fbf626ecc6452635de2b29224519b03b
Johannes Medagbe <johanmedagbe@gmail.com>
2026-06-15 18:39:52 +0300

    Fix: improve analysis with networkx - rename script from network_drawing.py to networkx.py - add logic to compute network connectivity metrics from the graph - plot network connectivity metrics results - save both network and connectivity results

--- scripts/networkx.py
+++ scripts/networkx.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--input-path", help="Path to file with network analysis metrics results"
     )
-    parser.add_argument("--output-path", help="Path to save network figure at")
+    parser.add_argument("--network-path", help="Path to save network figure at")
+    parser.add_argument("--metric-path", help="Path to save network figure at")
     args = parser.parse_args()
 
     with open(args.input_path) as i:
@@ -35,8 +36,8 @@ if __name__ == "__main__":
         edge_attr="weight",
         create_using=nx.DiGraph(),
     )
+    
     pos = nx.spiral_layout(G, resolution=0.6, scale=20, equidistant=True)
-
     plt.figure(figsize=(10, 10))
     nx.draw_networkx_nodes(G, pos, node_color="lightblue", node_size=1000)
     nx.draw_networkx_labels(G, pos, font_size=10, font_weight="bold")
@@ -51,5 +52,31 @@ if __name__ == "__main__":
 
     plt.title("Inferred Biological Connections in Gulf War Disease", fontsize=20)
     plt.axis("off")
-    plt.savefig(args.output_path, dpi=1000)
+    plt.savefig(args.network_path, dpi=1000)
     plt.show()
+
+    # Compute network connectivity metrics
+    degree_centrality = sorted(nx.degree_centrality(G).items(), key=lambda item: item[0])
+    betweenness_centrality = sorted(nx.betweenness_centrality(G).items(), key=lambda item: item[0])
+    eigenvector_centrality = sorted(nx.eigenvector_centrality(G, max_iter=1000).items(), key=lambda item: item[0])
+    degree_clustering = sorted(nx.clustering(G).items(), key=lambda item: item[0])
+
+    final_metrics = pd.DataFrame(
+        {
+            "Genes": list(dict(degree_centrality).keys()),
+            "degree_centrality": list(dict(degree_centrality).values()),
+            "betweenness_centrality": list(dict(betweenness_centrality).values()),
+            "eigenvector_centrality": list(dict(eigenvector_centrality).values()),
+            "degree_clustering": list(dict(degree_clustering).values()),
+        }
+    )
+    final_metrics.plot.bar(
+        x="Genes",
+        figsize=(10, 10),
+        title="Network connectivity metrics"
+    )
+    plt.savefig(args.metric_path, dpi=1000)
+    plt.show()
+
+
+        
